7fda691c4899fa1ac3a3fa10e2321e151edafa22
Sayam Sethi <sayam.sethi2@gmail.com>
2023-06-21 18:04:30 +0530

    update commit

---
+++ matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def unitary(M):
+    return M
+
+X = np.array([[0, 1], [1, 0]])
+Z = np.array([[1, 0], [0, -1]])
+Y = np.array([[0, -1j], [1j, 0]])
+I = np.array([[1, 0], [0, 1]])
